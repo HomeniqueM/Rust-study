2fa21caacca810842a6e59a702c577d7474368ed
Homenique Vieira <Homenique.t@gmail.com>
2022-06-23 19:41:43 -0300

    Finalizado a parte de cargo e rustfmt

---
+++ src/2.Cargo/projeto_01/target/debug/deps/projeto_01-6a884754c203598d.d
@@ -0,0 +1,5 @@
+/home/homenique/Data/Projetos/Rust-study/src/2.Cargo/projeto_01/target/debug/deps/projeto_01-6a884754c203598d: src/main.rs
+
+/home/homenique/Data/Projetos/Rust-study/src/2.Cargo/projeto_01/target/debug/deps/projeto_01-6a884754c203598d.d: src/main.rs
+
+src/main.rs:
---
+++ src/2.Cargo/projeto_01/target/debug/projeto_01.d
@@ -0,0 +1,1 @@
+/home/homenique/Data/Projetos/Rust-study/src/2.Cargo/projeto_01/target/debug/projeto_01: /home/homenique/Data/Projetos/Rust-study/src/2.Cargo/projeto_01/src/main.rs
