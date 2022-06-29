ad76af892eb4c86197c354327f3fae4fdd796759
Homenique Vieira <Homenique.t@gmail.com>
2022-06-29 00:39:09 -0300

    Criado a primeira parte do jogo de adviação assim como a sua entrada

--- src/jogo_de_advinhacao/src/main.rs
+++ src/jogo_de_advinhacao/src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Advinhe o número!");
+    println!("Digite o seu palpite.");
+    
+    let mut palpite = String::new();
+    io::stdin().read_line(&mut palpite).expect("Falha ao ler a entreada");
+
+    println!("Você digitou: {}",palpite);
 }
---
+++ src/jogo_de_advinhacao/target/debug/jogo_de_advinhacao.d
@@ -0,0 +1,1 @@
+/home/homenique/Data/Projetos/Rust-study/src/jogo_de_advinhacao/target/debug/jogo_de_advinhacao: /home/homenique/Data/Projetos/Rust-study/src/jogo_de_advinhacao/src/main.rs
